6bc8398ee97250e071d89d16789b4adaa06b6552
quangcn625 <quanglg2311@gmail.com>
2026-03-06 23:01:54 +0700

    update file constraint & library

--- constraint.js
+++ constraint.js
@@ -1,2 +1,9 @@
-export const TMDB_API_KEY = "da620413251351f77d93c17c62c2d352"
-export const URL = "https://api.themoviedb.org/3"
+export const TMDB_API_KEY = "da620413251351f77d93c17c62c2d352";
+export const URL = "https://api.themoviedb.org/3";
+export const tabs = [
+    { key: "now_playing", label: "Now playing" },
+    { key: "upcoming", label: "Upcoming" },
+    { key: "top_rated", label: "Top rated" },
+    { key: "popular", label: "Popular" },
+    { key: "end", label: "End" },
+];
